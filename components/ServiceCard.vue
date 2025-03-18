6e8fb2c0d15a3d153ef49c80c27dfcef38e26e68
pavezi <pavezivinicius@gmail.com>
2025-03-18 10:21:23 -0300

    feat: ServiceCard

---
+++ components/ServiceCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-card class="mb-4">
+        <v-card-title>{{ title }}</v-card-title>
+        <v-card-text>{{ description }}</v-card-text>
+    </v-card>
+</template>
+
+<script setup>
+defineProps({
+    title: String,
+    description: String,
+});
+</script>
