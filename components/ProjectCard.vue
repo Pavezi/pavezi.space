5a31836ad3492b817eac3b7be2888235cf241843
pavezi <pavezivinicius@gmail.com>
2025-03-17 22:22:06 -0300

    feat: ProjectCard

---
+++ components/ProjectCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-card class="project-card">
+        <v-img :src="image" height="200px" cover />
+        <v-card-title>{{ title }}</v-card-title>
+        <v-card-text>{{ description }}</v-card-text>
+        <v-card-actions>
+            <v-btn :href="link" target="_blank" color="primary" variant="outlined">
+                Ver Projeto
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script setup>
+defineProps({
+    title: String,
+    description: String,
+    image: String,
+    link: String
+});
+</script>
+
+<style scoped>
+.project-card {
+    width: 300px;
+    margin: 16px;
+}
+</style>
