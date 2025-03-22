3cfc46229faccb063b46bb6be81329b596a7b7bc
pavezi <pavezivinicius@gmail.com>
2025-03-22 19:28:05 -0300

    feat: Contact Page

---
+++ pages/contato.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1 class="text-h4 font-weight-bold mb-6">Entre em Contato </h1>
+        <ContactForm :initialSubject="subject" />
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const subject = ref('');
+
+onMounted(() => {
+    subject.value = route.query.assunto || '';
+});
+</script>
