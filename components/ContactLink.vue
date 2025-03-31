2a5f16f361f4c33b8081dfefd4eadae38b534af9
pavezi <pavezivinicius@gmail.com>
2025-03-31 09:47:43 -0300

    feat: ContactLink component

---
+++ components/ContactLink.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-btn :href="href" target="_blank" rel="noopener noreferrer" icon class="mx-2">
+        <v-icon :color="theme.global.current.value.dark ? 'primary' : 'secondary'">
+            {{ icon }}
+        </v-icon>
+    </v-btn>
+</template>
+
+<script setup>
+import { useTheme } from 'vuetify';
+
+const props = defineProps({
+    href: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: String,
+        required: true
+    }
+});
+
+const theme = useTheme();
+</script>
